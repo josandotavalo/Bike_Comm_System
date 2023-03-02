66b5ce53c2b6ae30095e8ff7956fa7ab6c800f71
José Andrés Otavalo Quito <71414651+josandotavalo@users.noreply.github.com>
2023-03-01 21:04:32 -0500

    Update batman_server.sh

--- batman_scripts/batman_server.sh
+++ batman_scripts/batman_server.sh
@@ -2,18 +2,16 @@
 
 # Activate batman-adv
 sudo modprobe batman-adv
+sudo systemctl daemon-reload
+sudo systemctl stop dhcpcd
 # Disable and configure wlan0
 sudo ip link set wlan0 down
 sudo ifconfig wlan0 mtu 1500
 sudo iwconfig wlan0 mode ad-hoc
 sudo iwconfig wlan0 essid "batman-network"
 sudo iwconfig wlan0 ap any
-sudo iwconfig wlan0 channel 1
-sleep 3s
+sudo iwconfig wlan0 channel 6
 sudo ip link set wlan0 up
-sleep 3s
 sudo batctl if add wlan0
-sleep 3s
 sudo ifconfig bat0 up
-sleep 3s
 sudo ifconfig bat0 10.0.0.1/24
