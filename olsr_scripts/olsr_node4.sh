9cec1811903d0fbb96fbeb45eb48263cf4208117
José Andrés Otavalo Quito <71414651+josandotavalo@users.noreply.github.com>
2023-03-06 21:07:53 -0500

    Update olsr_node4.sh

--- olsr_scripts/olsr_node4.sh
+++ olsr_scripts/olsr_node4.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Config mode ad-hoc
+# Configuracion modo ad-hoc
+sudo systemctl daemon-reload
 sudo systemctl stop dhcpcd
 sudo ip link set wlan1 down
 sudo ifconfig wlan1 mtu 1500
 sudo iwconfig wlan1 mode ad-hoc
-sudo iwconfig wlan1 essid "olsr-network"
+sudo iwconfig wlan1 essid "olsr_network"
 sudo iwconfig wlan1 ap any
 sudo iwconfig wlan1 channel 6
+sudo iwconfig wlan1 txpower 20
 sudo ip link set wlan1 up
 sudo ifconfig wlan1 20.0.0.14/24
 
-#start_olsrd
+# OLSRd
 sudo olsrd -i wlan1
