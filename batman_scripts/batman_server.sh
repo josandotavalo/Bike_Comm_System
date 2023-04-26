5a72cddef9fa10698461bb7101101797af37b245
José Andrés Otavalo Quito <71414651+josandotavalo@users.noreply.github.com>
2023-04-25 22:16:32 -0500

    Update batman_server.sh

--- batman_scripts/batman_server.sh
+++ batman_scripts/batman_server.sh
@@ -4,13 +4,14 @@
 #sudo systemctl daemon-reload
 #sudo systemctl stop dhcpcd
 sudo iwconfig wlan1 rate 11M
+#sudo iwconfig wlan1 modu 11b
 sudo ip link set wlan1 down
 sudo ifconfig wlan1 mtu 1500
 sudo iwconfig wlan1 mode ad-hoc
 sudo iwconfig wlan1 essid "batman_network"
 sudo iwconfig wlan1 ap any
-sudo iwconfig wlan1 channel 6
-sudo iwconfig wlan1 txpower 20
+sudo iwconfig wlan1 channel 2
+sudo iwconfig wlan1 txpower 0
 sudo ip link set wlan1 up
 
 # BATMAN-adv
