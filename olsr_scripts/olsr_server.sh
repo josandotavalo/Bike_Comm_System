adb1748d2a951510b86dc5d4de4733582a6c4241
José Andrés Otavalo Quito <71414651+josandotavalo@users.noreply.github.com>
2023-04-28 06:57:28 -0500

    Update olsr_server.sh

--- olsr_scripts/olsr_server.sh
+++ olsr_scripts/olsr_server.sh
@@ -3,15 +3,16 @@
 # Configuracion modo ad-hoc
 #sudo systemctl daemon-reload
 #sudo systemctl stop dhcpcd
-sudo iwconfig wlan1 rate 11M
+sudo iwconfig wlan1 rate 54M
 sudo ip link set wlan1 down
 sudo ifconfig wlan1 mtu 1500
 sudo iwconfig wlan1 mode ad-hoc
 sudo iwconfig wlan1 essid "olsr_network"
 sudo iwconfig wlan1 ap any
 sudo iwconfig wlan1 channel 2
-sudo iwconfig wlan1 txpower 0
 sudo ip link set wlan1 up
+sudo iwconfig wlan1 txpower 0dBm
+
 sudo ifconfig wlan1 20.0.0.1/24
 
 # OLSRd
