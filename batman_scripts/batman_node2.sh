c374d9de28c5a35cc57c115882f203ebfd1528fa
José Andrés Otavalo Quito <71414651+josandotavalo@users.noreply.github.com>
2023-04-28 06:55:14 -0500

    Update batman_node2.sh

--- batman_scripts/batman_node2.sh
+++ batman_scripts/batman_node2.sh
@@ -3,14 +3,15 @@
 # Configuracion modo ad-hoc
 sudo systemctl daemon-reload
 sudo systemctl stop dhcpcd
+sudo iwconfig wlan1 rate 54M
 sudo ip link set wlan1 down
 sudo ifconfig wlan1 mtu 1500
 sudo iwconfig wlan1 mode ad-hoc
 sudo iwconfig wlan1 essid "batman_network"
 sudo iwconfig wlan1 ap any
-sudo iwconfig wlan1 channel 6
-sudo iwconfig wlan1 txpower 20
+sudo iwconfig wlan1 channel 2
 sudo ip link set wlan1 up
+sudo iwconfig wlan1 txpower 20dBm
 
 # BATMAN-adv
 sudo modprobe batman-adv
